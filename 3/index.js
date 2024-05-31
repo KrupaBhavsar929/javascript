6500d709cf33e43b5e4a74f1f09d156cb3abfaae
KrupaBhavsar929 <bhavsarkrupa99@gmail.com>
2024-06-01 00:12:07 +0530

    Learn Loops and Strings

---
+++ 3/index.js
@@ -0,0 +1,38 @@
+for(let i=1;i<=10;i++)
+    {
+        console.log(" hello kitty");
+    }
+
+    // print all the even num from  0 to 100
+
+    for(let i=0;i<=100;i++)
+        {
+            if(i%2==0)
+                {
+                    console.log(i);
+                }
+
+        }
+
+        // game number
+let game=9;
+let p;
+            
+
+while(game!=p)
+{
+    p=prompt("guess the number: ");
+    if(game==p)
+    
+    console.log(" you guess correct number ",game);
+}
+
+let string="hello";
+console.log(string.length);
+console.log(string[1]);
+
+let username=prompt("enter the username");
+let output=`@${username}13`;
+console.log(output);
+
+        
