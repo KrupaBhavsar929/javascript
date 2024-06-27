6e355e1820a142b819a3040faffcbe026c92f7d0
KrupaBhavsar929 <bhavsarkrupa99@gmail.com>
2024-06-27 12:18:55 +0530

    DOM practice

--- 6/script.js
+++ 6/script.js
@@ -1,41 +1,61 @@
-// alert("hello dear !!!");
+// // alert("hello dear !!!");
 
-// console.dir(document.head);
-//  let a=document.getElementsByClassName("heading");
+// // console.dir(document.head);
+// //  let a=document.getElementsByClassName("heading");
  
-// console.dir(a);
-// console.log(a);
+// // console.dir(a);
+// // console.log(a);
 
-let p1=document.getElementsByTagName("p");
-console.dir(p1);
+// let p1=document.getElementsByTagName("p");
+// console.dir(p1);
 
-let firstEle = document.querySelector("#button");
-console.dir(firstEle);
+// let firstEle = document.querySelector("#button");
+// console.dir(firstEle);
 
-let Ele = document.querySelector(".heading");
-console.dir(Ele);
+// let Ele = document.querySelector(".heading");
+// console.dir(Ele);
 
-let allEle = document.querySelectorAll(".heading");
-console.dir(allEle);
+// let allEle = document.querySelectorAll(".heading");
+// console.dir(allEle);
 
- let f=document.querySelector(".fruits")
-
-
-//  exclude hidden content
- console.dir(f.innerText);
- console.dir(f.innerHTML);
- console.dir(f.textContent);
-h3=document.querySelector("h3")
-console.log(h3);
-console.log(h3.innerHTML);
-console.log(h3.innerText);
- 
-hello=document.getElementById("hello");
-console.log(hello.innerText +" from Apna College students");
+//  let f=document.querySelector(".fruits")
 
 
-div=document.getElementsByClassName("box");
-for(d of div)
-    {
-        console.log(d.innerText);
-    }
+// //  exclude hidden content
+//  console.dir(f.innerText);
+//  console.dir(f.innerHTML);
+//  console.dir(f.textContent);
+// h3=document.querySelector("h3")
+// console.log(h3);
+// console.log(h3.innerHTML);
+// console.log(h3.innerText);
+  
+
+// hello=document.getElementById("hello");
+// console.log(hello.innerText +" from Apna College students");
+
+
+// div=document.getElementsByClassName("box");
+// for(d of div)
+//     {
+//         console.log(d.innerText);
+//     }   
+
+// const t = document.getElementById("hello");
+// console.log("it give first tag",t.firstElementChild);
+
+// console.log("it give  first content information",t.firstChild);
+
+// console.log("it give  last content information",t.lastChild);
+// console.log(t.firstElementChild.innerText);
+// console.log(t.lastElementChild.innerText);
+// console.log("USING INNER HTML ",t.firstElementChild.innerHTML);
+// console.log("USING TEXT CONTENT ",t.firstElementChild.textContent);
+// console.log("=====")
+
+const t1= document.getElementsByClassName("box");
+console.log("text content:",t1[1].textContent);
+console.log("inner text ",t1[1].innerText[]);
+console.log("inner tag: ", t1[1]);
+
+
---
+++ 8/script.js
@@ -0,0 +1,17 @@
+ let div =document.querySelector("div");
+ console.log(div);
+
+  let id=div.getAttribute("id");
+  console.log(id);
+  let div1 =document.getElementById("box");
+  console.log(div);
+
+  console.log(div1.innerText);
+
+
+
+
+  
+  let para1=document.querySelector("p");
+  let a = para1.getAttribute("id");
+  console.log(a);
